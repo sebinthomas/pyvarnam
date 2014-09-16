8ee34ef4bc1c4ffcc3b52ce3419f6abe6d1a3d8b
sebin thomas <stc043@gmail.com>
2014-09-17 01:10:17 +0530

    Added varray , token and word definitions
    Added class definitions for varray, token and word.
    Rectified some mistakes because of the '__'
    (double underscore) with  _fields_ . Thinking of
    parallel development of the main user facing library
    along with adding functions.

--- varnam.py
+++ varnam.py
@@ -1,16 +1,44 @@
 #!usr/bin/env python
-""" Varnam constants and function lists
+""" Varnam library functions list
 """
-# Varnam constants and function lists
+# Varnam library functions list
 from utils import *
 import ctypes as C
 
-class Varnam(C.Structure):
-    __fields__ = [('scheme_file', STRING),
-                  ('suggestions_file', STRING),
-                  ('internal', VOID)]
 
-VARNAM_PTR = C.POINTER(Varnam)
+class VarnamHandle(C.Structure):
+    _fields_ = [('scheme_file', STRING),
+                ('suggestions_file', STRING),
+                ('internal', VOID)]
+
+VARNAM_PTR = C.POINTER(VarnamHandle)
+
+class Varray(C.Structure):
+    _fields_ = [('memory', C.POINTER(VOID)),
+                ('allocated', C.c_size_t),
+                ('used', C.c_size_t),
+                ('index', INT)]
+
+
+class Token(C.Structure):
+    _fields_ = [('id', INT),
+                ('type', INT),
+                ('match_type', INT),
+                ('priority', INT),
+                ('accept_condition', INT),
+                ('flags', INT),
+                ('tag', STRING),
+                ('pattern', STRING),
+                ('value1', STRING),
+                ('value2', STRING),
+                ('value3', STRING)]
+
+    
+class Word(C.Structure):
+    _fields_ = [('text', STRING),
+                ('confidence', INT)]
 
 FUNCTION_LIST = [
-    ('varnam_init', (STRING, C.POINTER(VARNAM_PTR), C.POINTER(STRING)), INT)]
+    ('varnam_init', (STRING, C.POINTER(VARNAM_PTR), C.POINTER(STRING)), INT),
+    ('varnam_init_from_lang', (STRING, C.POINTER(VARNAM_PTR), C.POINTER(STRING)), INT),
+    ('varnam_version', (), STRING)]
