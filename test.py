04a013f44d7eb34a09c7ed257decbeb2c78a8035
sebin thomas <stc043@gmail.com>
2014-11-02 01:22:56 +0530

    Fixed garbled text output
    Garbled text was a real pain. One suggestion was to use UTF-8 to
    decode strings returned by libvarnam. Libvarnam uses UTF-8 encoding
    to encode return text. This, although useful, wasn't the root cause.
    Garbled text kept on changing after each run which pointed to a
    memory issue.
    
    Turns out that the returned varray pointer was not
    being properly handled. I casted the pointer to appropriate type
    and then used it and voila, it works !
    
    Added another test case to check the malayalam output.

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 #!usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # really quick and dirty test to get across the point
 # that nothing is really working
@@ -13,53 +14,86 @@ import ctypes as C
 import unittest
 import os
 
-FILE_NAME = "testfile"
+# Test to check if malayalam symbol table is working
+TEST_WORD = u"ivan"
+TEST_OP = u"ഇവൻ"
+TEST_SIZE = 1
 
-# there is probably no need for this
-# but it is late and I can't think
-def get_a_file(name):
-    file_handle = open(name, 'a')
-    return file_handle
+
 
 class VarnamTrans(unittest.TestCase):
     """ Check the transliteration by setting up some tokens
     """
     def setUp(self):
         self.lib = InternalVarnamLibrary()
-        self.handle = VARNAM_PTR()
+        self.handle_object = VarnamHandle()
+        self.handle = C.pointer(self.handle_object)
         msg = STRING()
-        self.scheme_file = get_a_file(FILE_NAME)
-        init_return = self.lib.varnam_init(FILE_NAME, C.byref(self.handle), C.byref(msg))
+        init_return = self.lib.varnam_init("", C.byref(self.handle), C.byref(msg))
         assert init_return is VARNAM_SUCCESS
+        # Creates 3 tokens for testing purposes
+        rcode = self.lib.varnam_create_token(self.handle, u"a", u"a-value1", u"a-value2",
+                                          u"", u"", VARNAM_TOKEN_VOWEL, VARNAM_MATCH_EXACT, 0, 0, 0)
+        self.assertEqual(rcode, VARNAM_SUCCESS)
+        rcode = self.lib.varnam_create_token(self.handle, u"aa", u"aa-value1", u"aa-value2",
+                                          u"", u"", VARNAM_TOKEN_VOWEL, VARNAM_MATCH_EXACT, 0, 0, 0)
+        self.assertEqual(rcode, VARNAM_SUCCESS)
+        rcode = self.lib.varnam_create_token(self.handle, u"e", u"e-value1", u"e-value2",
+                                          u"", u"", VARNAM_TOKEN_VOWEL, VARNAM_MATCH_EXACT, 0, 0, 0)
+        self.assertEqual(rcode, VARNAM_SUCCESS)
+        rcode = self.lib.varnam_create_token(self.handle, u"k", u"k-value1", u"k-value2",
+                                          u"", u"", VARNAM_TOKEN_CONSONANT, VARNAM_MATCH_EXACT, 0, 0, 0)
+        self.assertEqual(rcode, VARNAM_SUCCESS)
 
-    def test_create_token(self):
-        """ This test creates three tokens
-        """
-        rcode = self.lib.varnam_create_token(self.handle, "a", "a-value1", "a-value2",
-                                          "", "", VARNAM_TOKEN_VOWEL, VARNAM_MATCH_EXACT, 0, 0, 0)
-        self.assertEqual(rcode, VARNAM_SUCCESS)
-        rcode = self.lib.varnam_create_token(self.handle, "e", "e-value1", "e-value2",
-                                          "", "", VARNAM_TOKEN_VOWEL, VARNAM_MATCH_EXACT, 0, 0, 0)
-        self.assertEqual(rcode, VARNAM_SUCCESS)
-        rcode = self.lib.varnam_create_token(self.handle, "k", "k-value1", "k-value2",
-                                          "", "", VARNAM_TOKEN_CONSONANT, VARNAM_MATCH_EXACT, 0, 0, 0)
-        self.assertEqual(rcode, VARNAM_SUCCESS)
-
-    def test_transliterate(self):
+    def test__basic_transliteration(self):
         """ Test to transliterate the tokens we created earlier
         """
-        words = VARRAY_PTR()
-        rcode = self.lib.varnam_transliterate(self.handle, "aek", C.byref(words))
+        varray_object = Varray()
+        varray = C.pointer(varray_object)
+        rcode = self.lib.varnam_transliterate(self.handle, u"aek", C.byref(varray))
         self.assertEqual(rcode, VARNAM_SUCCESS)
-        self.assertEqual(self.lib.varray_length(words), 1)
-        single_word = Word(self.lib.varray_get(words, 0))
-        print single_word.text
-        self.assertEqual(single_word.text, "a-value1e-value2k-value1")
+        self.assertEqual(self.lib.varray_length(varray), 1)
+        vword_pointer = C.cast(self.lib.varray_get(varray, 0), C.POINTER(Word))
+        vword = vword_pointer.contents
+        vword.text = vword.text.decode('utf-8')
+        print "value returned by transliterate function: %s"%vword.text
+        self.assertEqual(unicode(vword.text), u"a-value1e-value2k-value1")
 
     def tearDown(self):
         self.lib.varnam_destroy(self.handle)
-        self.scheme_file.close()
-        os.remove(FILE_NAME)
+
+
+
+class VarnamMLTrans(unittest.TestCase):
+    """
+    Translates a malayalam word ഇവൻ.
+    Mainly for seeing if it does transliterate.
+    TODO: remove this test.
+    """
+    def setUp(self):
+        self.lib = InternalVarnamLibrary()
+        self.handle_object = VarnamHandle()
+        self.handle = C.pointer(self.handle_object)
+        msg = STRING()
+        init = self.lib.varnam_init_from_lang('ml', C.byref(self.handle), C.byref(msg))
+        assert init is VARNAM_SUCCESS
+
+    def test_transliterate(self):
+        """
+        deals with transliteration and shows you the O/p
+        """
+        varray_object = Varray()
+        varray = C.pointer(varray_object)
+        rcode = self.lib.varnam_transliterate(self.handle, TEST_WORD, C.byref(varray))
+        assert rcode is VARNAM_SUCCESS
+        size = self.lib.varray_length(varray)
+        assert size is TEST_SIZE
+        vword_pointer = C.cast(self.lib.varray_get(varray, 0), C.POINTER(Word))
+        vword = vword_pointer.contents
+        word = vword.text.decode('utf-8')
+        print "word is %s and confidence is %d"%(word, vword.confidence)
+        self.assertEqual(word, TEST_OP)
+
 
 if __name__ == '__main__':
     unittest.main()
