60bfba5dbf21a91d18ef3c19f7eca09a5819964e
sebin thomas <stc043@gmail.com>
2014-12-18 01:21:24 +0530

    Added more function wrappers to varnam
    Function wrappers for some of the functions were added.
    varnam_transliterate now returns a list of the transliterated words
    in utf-8 as well as their confidences.
    TODO: return the error code

--- varnam.py
+++ varnam.py
@@ -18,12 +18,47 @@ class Varnam:
         self.handle = C.pointer(self.__handle_obj)
         self.message = STRING()
 
-    def init(self, scheme_file):
+    def varnam_init(self, scheme_file):
         """
         function to initialize varnam handle
         scheme_file: valid scheme file(*.vst) path
         """
-        scheme_file_ptr = STRING(scheme_file)
         return self.lib.varnam_init(
-            scheme_file_ptr.value, C.byref(self.handle),
+            scheme_file, C.byref(self.handle),
             C.byref(self.message))
+
+    def varnam_init_from_lang(self, lang_code):
+        """
+        Initializes the varnam library from language code
+        lang_code: language code in ISO 639-1 format
+        """
+        return self.lib.varnam_init_from_lang(
+            lang_code, C.byref(self.handle),
+            C.byref(self.message))
+
+    def varnam_version(self):
+        """
+        Returns the version of libvarnam
+        """
+        return self.lib.varnam_version()
+
+    def varnam_transliterate(self, input):
+        """
+        Performs transliteration on a given input
+        Returns: List of transliterations encoded in UTF-8
+        input: input word to transliterate
+        """
+        varray_object = Varray()
+        varray_ptr = C.pointer(varray_object)
+        res_code = self.lib.varnam_transliterate(self.handle,
+                                               input,
+                                               C.byref(varray_ptr))
+        result = []
+        length = self.lib.varray_length(varray_ptr)
+        for i in range(length):
+            word_ptr = C.cast(self.lib.varray_get(varray_ptr, i),
+                              C.POINTER(Word))
+            result.append((word_ptr.contents.text,
+                           word_ptr.contents.confidence))
+        return result
+        
